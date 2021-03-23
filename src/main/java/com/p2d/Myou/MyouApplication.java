8141342463203c97d9262492759e23d4bb81ef7d
Lucas-Bignon <72074469+Lucas-Bignon@users.noreply.github.com>
2021-03-23 13:25:04 +0100

    Initial

---
+++ src/main/java/com/p2d/Myou/MyouApplication.java
@@ -0,0 +1,13 @@
+package com.p2d.Myou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p2d/Myou/MyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2d.Myou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
